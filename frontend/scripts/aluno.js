52d0c19bcb0c49224d227d26e860daea631ec770
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2025-10-06 22:04:39 -0300

    feat(aluno): Exibindo em forma de cards

--- frontend/scripts/aluno.js
+++ frontend/scripts/aluno.js
@@ -1,14 +1,17 @@
-const HOST = 'http://localhost:8080';
+const SERVER = 'http://localhost:8080';
 
 async function getAlunos() {
 
-    const respostaAPI = await fetch(`${HOST}/aluno`);
+    const respostaAPI = await fetch(`${SERVER}/aluno`);
     const alunoList = await respostaAPI.json();
 
     const divAlunoList = document.getElementById('alunoList');
 
+    divAlunoList.innerHTML = '';
     if (alunoList.length > 0) {
-        divAlunoList.innerHTML = alunoList;
+        for (let aluno of alunoList) {
+            divAlunoList.innerHTML += `<div class="card">${aluno}</div>`;
+        }
     } else {
         divAlunoList.innerHTML = '<p>Nenhum registro de aluno encontrado!</p>';
     }
@@ -16,16 +19,20 @@ async function getAlunos() {
 
 }
 
-getAlunos();
-
 async function adicionarAluno() {
 
     const aluno = document.getElementById('aluno').value;
 
-    const response = await fetch(`${HOST}/aluno/${aluno}`);
-    const mensagemCadastro = await response.json();
+    const response = await fetch(`${SERVER}/aluno/${aluno}`);
+    const mensagemCadastro = await response.text();
 
     alert(mensagemCadastro);
+    document.getElementById('aluno').value = '';
     getAlunos();
 
 }
+
+getAlunos();
+
+// EXERCÍCIO
+// Renderizar os nomes dos alunos em forma de cards
